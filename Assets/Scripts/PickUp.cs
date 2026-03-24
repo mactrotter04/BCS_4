2486c4b93751f21ee8fa77abaf452c6e06930fe2
mactrotter04 <mactrotter04@gmail.com>
2026-03-24 11:23:23 +0100

    changed issue with collecting multiple coins

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -5,11 +5,15 @@ public class PickUp : MonoBehaviour
     [SerializeField] AudioClip pickupSFX;
     [SerializeField] int pointsForCoinPickup = 100;
 
+    bool wasCollected = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player") && !wasCollected)
         {
+            wasCollected = true;
             FindFirstObjectByType<GameSession>().AddToScore(pointsForCoinPickup);
+            gameObject.SetActive(false);
             AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
