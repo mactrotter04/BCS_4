6948fad438ad0121d0613d56f2e70605b5f9bf2a
mactrotter04 <mactrotter04@gmail.com>
2026-03-25 10:55:28 +0100

    burst issues
    burst problems

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,34 +1,108 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    [Header ("Exit")]
+    [SerializeField] float exitTime = 5f;
+    [SerializeField] TextMeshPro extractionTimerText;
+
+
+    bool InsideExit;
+    Coroutine extractionCoroutine;
+    float currentTime;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            int currntSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            int nextSceneIndex = currntSceneIndex + 1;
+            InsideExit = true;
 
-            if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            if (extractionCoroutine == null)
             {
-                Debug.Log("no more scenes to load");
+                extractionCoroutine = StartCoroutine(updateTimer());
+                extractionCoroutine = null;
             }
 
-            FindFirstObjectByType<ScenePersist>().ResetScenePersist();
-            SceneManager.LoadScene(nextSceneIndex);
+            currentTime = exitTime;
+            UpdateTimerText();
         }
     }
+        
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            InsideExit = false;
+
+            if (extractionCoroutine != null)
+            {
+                StopCoroutine(updateTimer());
+                extractionCoroutine = null;
+            }
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        int currntSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currntSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("no more scenes to load");
+
+        }
+
+        FindFirstObjectByType<ScenePersist>().ResetScenePersist();
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    IEnumerator updateTimer()
+    {
+        currentTime = exitTime; //5s
+        UpdateTimerText();
+
+        while (currentTime > 0f) //while (true)
+        {
+            if(InsideExit)
+            {
+                yield break;
+            }
+
+            currentTime -= Time.deltaTime;
+
+            if(currentTime < 0f)
+            {
+                currentTime = 0f;
+            }
+
+            UpdateTimerText();
+            yield return null;
+        }
+
+        extractionCoroutine = null;
+        LoadNextLevel();
+    }
+
+    void UpdateTimerText()
+    {
+        extractionTimerText.text = "Extraction in: " + currentTime.ToString();
+    }
+
 }
